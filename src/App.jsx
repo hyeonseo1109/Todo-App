92bb24c52dd15c17501122389e01b07b4190199b
hyeonseo1109 <alsgustj2@naver.com>
2025-07-02 14:38:05 +0900

    Todo-App 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "./App.css";
 
 function App() {
@@ -21,12 +21,15 @@ function TodoInput({ todoList, setTodoList }) {
   const [inputValue, setInputValue] = useState("");
 
   return (
-    <>
+    <div className="plus">
       <input
+        className="plusInput"
         value={inputValue}
         onChange={(event) => setInputValue(event.target.value)}
       />
+      
       <button
+        className="plusBtn"
         onClick={() => {
           const newTodo = { id: Number(new Date()), content: inputValue };
           const newTodoList = [...todoList, newTodo];
@@ -34,9 +37,9 @@ function TodoInput({ todoList, setTodoList }) {
           setInputValue("");
         }}
       >
-        추가하기
+        추가
       </button>
-    </>
+    </div>
   );
 }
 
@@ -52,33 +55,61 @@ function TodoList({ todoList, setTodoList }) {
 
 function Todo({ todo, setTodoList }) {
   const [inputValue, setInputValue] = useState("");
+  const inputRef = useRef(null);
+  const radioBtn = useRef(null);
+  const clickRadio = () => {
+    const radio = radioBtn.current;
+    radio.classList.toggle("line");
+  }
+  const handleEditClick = () => {
+    const input = inputRef.current;
+    input.classList.toggle("hidden");
+
+    if (!input.classList.contains("hidden")) {
+    input.focus();
+    input.value = todo.content;
+    } else {
+      const newValue = input.value;
+      setTodoList((prev) =>
+        prev.map((el) =>
+          el.id === todo.id ? { ...el, content: newValue } : el
+        )
+      );
+    }
+  };
   return (
-    <li>
-      {todo.content}
-      <input
-        value={inputValue}
-        onChange={(event) => setInputValue(event.target.value)}
-      />
-      <button
-        onClick={() => {
-          setTodoList((prev) =>
-            prev.map((el) =>
-              el.id === todo.id ? { ...el, content: inputValue } : el
-            )
-          );
-        }}
-      >
-        수정
-      </button>
-      <button
-        onClick={() => {
-          setTodoList((prev) => {
-            return prev.filter((el) => el.id !== todo.id);
-          });
-        }}
-      >
-        삭제
-      </button>
+    <li ref={radioBtn}>
+      <div className="div">
+        <button
+          onClick={clickRadio}
+          className="finishBtn"
+        >완료</button>
+        {todo.content}
+      </div>
+
+      <div className="buttons">
+        <input
+          ref={inputRef}
+          className="hidden"
+          value={inputValue}
+          onChange={(event) => setInputValue(event.target.value)}
+        />
+        <button 
+          onClick= {handleEditClick}
+          className="btn"> 
+          수정
+        </button>
+        <button
+          onClick={() => {
+            setTodoList((prev) => {
+              return prev.filter((el) => el.id !== todo.id);
+            });
+          }}
+          className="btn"
+        >
+          삭제
+        </button>
+      </div>
     </li>
   );
 }
